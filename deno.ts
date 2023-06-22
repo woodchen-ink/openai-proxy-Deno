e4ed00dfc16038c5da9a3b5b37c49b735c7a9b1f
wood chen <95951386+woodchen-ink@users.noreply.github.com>
2023-06-22 19:36:49 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -5,10 +5,6 @@ const OPENAI_API_HOST = "api.openai.com";
 serve(async (request) => {
   const url = new URL(request.url);
 
-  if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
-  }
-
   url.host = OPENAI_API_HOST;
   return await fetch(url, request);
 });
